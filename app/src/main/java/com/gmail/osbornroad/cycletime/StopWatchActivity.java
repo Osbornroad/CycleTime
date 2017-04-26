275e38281cefbc9a5a9f5ed7d01cad2d935b51a9
Maksim Tkachenko <osbornroad@gmail.com>
2017-04-26 15:36:24 +0300

    Back from employee fixed

--- app/src/main/java/com/gmail/osbornroad/cycletime/StopWatchActivity.java
+++ app/src/main/java/com/gmail/osbornroad/cycletime/StopWatchActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -86,8 +87,13 @@ public class StopWatchActivity extends AppCompatActivity {
                 startActivityForResult(intent, 0);
             }
         });
-//        setSampleInfo();
+        setSampleInfo(savedInstanceState);
         stopWatchResume();
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onRestoreInstanceState(savedInstanceState, persistentState);
     }
 
     @Override
@@ -95,11 +101,13 @@ public class StopWatchActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
         outState.putBoolean("mStarted", mStarted);
         outState.putBoolean("mInProgress", mInProgress);
+        if (selectedEmployee != null) {
+            outState.putInt("employeeId", selectedEmployee.getId());
+        }
     }
 
     @Override
     protected void onRestart() {
-//        setSampleInfo();
         super.onRestart();
     }
 
@@ -125,17 +133,23 @@ public class StopWatchActivity extends AppCompatActivity {
         }
     }
 
-/*
-    private void setSampleInfo() {
-        Intent intent = getIntent();
-        if (intent.hasExtra("employeeId")) {
-            int employeeId = intent.getExtras().getInt("employeeId");
-            selectedEmployee = employeeService.get(employeeId);
-            employeeInfo.setText(selectedEmployee.getEmployeeName());
+    private void setSampleInfo(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey("mStarted")) {
+                mStarted = savedInstanceState.getBoolean("mStarted");
+            }
+            if (savedInstanceState.containsKey("mInProgress")) {
+                mInProgress = savedInstanceState.getBoolean("mInProgress");
+            }
+            stopWatchResume();
+            if (savedInstanceState.containsKey("employeeId")) {
+                int id = savedInstanceState.getInt("employeeId");
+                selectedEmployee = employeeService.get(id);
+                employeeInfo.setText(selectedEmployee.getEmployeeName());
+            }
         }
     }
 
-*/
 
     Handler mHandler = new Handler() {
         @Override
@@ -167,9 +181,9 @@ public class StopWatchActivity extends AppCompatActivity {
 
 
     private void stopWatchResume() {
-        mHandler.sendEmptyMessage(MSG_UPDATE);
         if (mStarted) {
             if (mInProgress) {
+                mHandler.sendEmptyMessage(MSG_UPDATE);
                 startButton.setText(R.string.button_stop);
             } else {
                 startButton.setText(R.string.button_resume);
